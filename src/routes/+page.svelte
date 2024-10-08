d5cea6ac153fdfe16f11d79758fb097352515143
John Gicharu <gicharucode@gmail.com>
2024-10-08 14:01:01 +0300

    feat: improve colors + change logic for clean or final trims

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,40 +1,63 @@
 <script lang="ts">
 	import { writable } from 'svelte/store';
 
-	const keep_amount = writable<number>(0);
-	const keep_volume = writable<number>(0);
-	const volume_type = writable<'UNITS' | 'LOTS'>('UNITS');
-	const close_partial_volume = writable<number>(0);
-	const loss_volume = writable<number>(0);
-	const loss_amount = writable<number>(0);
-	const profit_amount = writable<number>(0);
-	const profit_volume = writable<number>(0);
+	const keep_amount = writable<number>();
+	const keep_pips = writable<number>();
+	const pip_value = writable<number>(10);
+	const volume_type = writable<'UNITS' | 'LOTS'>('LOTS');
+	const close_partial_volume = writable<number>();
+	const loss_volume = writable<number>();
+	const loss_amount = writable<number>();
+	const profit_amount = writable<number>();
+	const profit_volume = writable<number>();
 
-	$: if ($keep_volume && $profit_amount && $loss_amount && $loss_volume && $profit_volume) {
-		keep_amount.set($profit_volume * ($keep_volume / ($volume_type === 'UNITS' ? 100000 : 1)));
-		const apply =
-			$profit_amount - $profit_volume * ($keep_volume / ($volume_type === 'UNITS' ? 100000 : 1));
+	$: if ($keep_pips && $profit_amount && $loss_amount && $loss_volume && $profit_volume) {
+		const amount_to_keep =
+			($profit_volume / ($volume_type === 'UNITS' ? 100000 : 1)) * $pip_value * $keep_pips;
+
+		keep_amount.set(Math.floor(amount_to_keep * 100) / 100);
+		const apply = $profit_amount - amount_to_keep;
 		close_partial_volume.set(
-			(apply / Math.abs($loss_amount)) * ($loss_volume / ($volume_type === 'UNITS' ? 100000 : 1))
+			Math.floor(
+				(apply / Math.abs($loss_amount)) *
+					($loss_volume / ($volume_type === 'UNITS' ? 100000 : 1)) *
+					100
+			) / 100
 		);
 	}
 </script>
 
-<main class="content">
-	<header class="flex items-center justify-center w-full px-3 py-5">
-		<h1 class="h-full text-2xl font-semibold whitespace-nowrap">Trim The Hedge Calculator</h1>
+<main class="bg-slate-100">
+	<header
+		class="flex flex-col items-center justify-center w-full px-3 py-3 space-y-2 text-slate-200 bg-slate-800"
+	>
+		<div class="logo">
+			<img src="ssfx.jpg" alt="SSFx" class="w-8 h-8 rounded-full" />
+		</div>
+		<h1 class="flex flex-col items-center justify-center h-full whitespace-nowrap">
+			<div class="text-2xl font-semibold heading_text">Trim The Hedge Calculator</div>
+			<div class="text-xs tagline">
+				made with love <a class="text-cyan-300" href="http://gicharu.com">@gicharu</a>
+			</div>
+		</h1>
 	</header>
 
 	<section class="calculator">
 		<div class="input_wrapper">
 			<label for="keep_pips">Keep Pips</label>
-			<input type="number" name="keep_pips" id="keep_pips" bind:value={$keep_volume} />
+			<input type="number" name="keep_pips" id="keep_pips" bind:value={$keep_pips} />
 		</div>
 
 		<div class="input_wrapper">
+			<label for="pip_value">Pip Value in Account Currency</label>
+			<input type="number" name="pip_value" id="pip_value" bind:value={$pip_value} />
+		</div>
+
+		<div class="input_wrapper">
+			<label for="volume">Volume Type</label>
 			<select name="volume" id="volume" bind:value={$volume_type}>
+				<option value="LOTS">LOTS</option>
 				<option value="UNITS">UNITS</option>
-				<option value="VOLUME">VOLUME</option>
 			</select>
 		</div>
 
@@ -44,7 +67,7 @@
 		</div>
 
 		<div class="input_wrapper">
-			<label for="profit_volume">Profit Volume</label>
+			<label for="profit_volume">Profit Volume ({$volume_type.toLowerCase()})</label>
 			<input type="number" name="profit_volume" id="profit" bind:value={$profit_volume} />
 		</div>
 
@@ -54,19 +77,51 @@
 		</div>
 
 		<div class="input_wrapper">
-			<label for="loss_volume">Loss Volume</label>
+			<label for="loss_volume">Loss Volume ({$volume_type.toLowerCase()})</label>
 			<input type="number" name="loss_volume" id="loss_volume" bind:value={$loss_volume} />
 		</div>
 
-		{#if $keep_volume && $profit_amount && $loss_amount && $loss_volume && $profit_volume}
-			<div class="summary_section">
-				<div class="keep_amount">Keep Amount: {$keep_amount}</div>
-				<div class="close_partial">Close Partial: {$close_partial_volume}</div>
-				<div class="close_partial">
-					Close Percentage: {($close_partial_volume /
-						($loss_volume / ($volume_type === 'UNITS' ? 100000 : 1))) *
-						100}%
+		{#if $keep_pips && $profit_amount && $loss_amount && $loss_volume && $profit_volume}
+			<div class="py-4 summary_section text-slate-200 bg-slate-800">
+				<div class="keep_amount">
+					<div class="font-semibold">Keep Amount:</div>
+					<div class="content">
+						{$keep_amount}
+					</div>
 				</div>
+				<div class="keep_amount">
+					<div class="font-semibold">Apply Amount:</div>
+					<div class="content">
+						{+($profit_amount - $keep_amount).toFixed(2)}
+					</div>
+				</div>
+				<div class="close_partial">
+					<div class="font-semibold">Close Partial:</div>
+					<div class="content">
+						{$close_partial_volume > $loss_volume ? $loss_volume : $close_partial_volume}
+					</div>
+				</div>
+				<div class="close_partial">
+					<div class="font-semibold">Close Percentage:</div>
+					<div class="content">
+						{$close_partial_volume > $loss_volume
+							? 100
+							: +(
+									($close_partial_volume /
+										($loss_volume / ($volume_type === 'UNITS' ? 100000 : 1))) *
+									100
+								).toFixed(2)}%
+					</div>
+				</div>
+
+				{#if $close_partial_volume > $loss_volume}
+					<div class="remaining_profit">
+						<div class="font-semibold">Apply to Next Trade:</div>
+						<div class="content">
+							{+($profit_amount - ($keep_amount + $loss_amount)).toFixed(2)}
+						</div>
+					</div>
+				{/if}
 			</div>
 		{/if}
 	</section>
@@ -84,19 +139,23 @@
 			}
 
 			input {
-				@apply px-3 border py-1 w-full;
+				@apply px-3 border py-1 w-full bg-gray-200;
 			}
 
 			select {
-				@apply w-full px-3 py-2;
+				@apply w-full px-3 py-2 bg-gray-200;
 			}
 		}
 
 		.summary_section {
 			@apply w-full px-4 py-2;
 
-			div {
-				@apply font-medium;
+			& > div {
+				@apply font-medium flex items-center justify-between;
+			}
+
+			.content {
+				@apply text-cyan-400;
 			}
 		}
 	}
